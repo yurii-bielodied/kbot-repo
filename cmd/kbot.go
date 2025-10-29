7f593dfec04966cdb4c7d14c2c9d24e9f6558294
Yurii Bielodied <ybeloded@gmail.com>
2025-10-29 13:54:26 +0200

    Code update

--- cmd/kbot.go
+++ cmd/kbot.go
@@ -1,4 +1,4 @@
-package main
+package cmd
 
 import (
 	"fmt"
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/spf13/cobra"
-	"github.com/stianeikeland/go-rpio/v4"
 	telebot "gopkg.in/telebot.v4"
 )
 
@@ -20,14 +19,11 @@ var (
 var kbotCmd = &cobra.Command{
 	Use:     "kbot",
 	Aliases: []string{"start"},
-	Short:   "Telegram bot for infra",
-	Long: `A Telegram bot that allows controlling infra.
-The bot accepts commands to toggle red, amber, and green status lights.
+	Short:   "Telegram bot",
+	Long: `A Telegram bot.
+The bot accepts some commands.
 
 Usage:
-  /s red    - Toggle red light
-  /s amber  - Toggle amber light
-  /s green  - Toggle green light
   hello     - Get a greeting from the bot`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if TeleToken == "" {
@@ -46,24 +42,6 @@ Usage:
 			log.Fatalf("Please check TELE_TOKEN env variable. %s", err)
 		}
 
-		if err := rpio.Open(); err != nil {
-			log.Fatalf("Unable to open GPIO: %s", err)
-		}
-		defer rpio.Close()
-
-		// Initialize traffic signals
-		trafficSignals := map[string]*TrafficSignal{
-			"red":   {Pin: 12, On: false},
-			"amber": {Pin: 27, On: false},
-			"green": {Pin: 22, On: false},
-		}
-
-		// Initialize all pins as input
-		for _, signal := range trafficSignals {
-			pin := rpio.Pin(signal.Pin)
-			pin.Input()
-		}
-
 		kbot.Handle(telebot.OnText, func(m telebot.Context) error {
 			log.Printf("Received message: %s", m.Text())
 			payload := m.Message().Payload
@@ -72,24 +50,8 @@ Usage:
 			case "hello":
 				return m.Send(fmt.Sprintf("Hello I'm Kbot %s!", appVersion))
 
-			case "red", "amber", "green":
-				signal := trafficSignals[payload]
-				pin := rpio.Pin(signal.Pin)
-
-				if !signal.On {
-					pin.Output()
-					pin.High()
-					signal.On = true
-				} else {
-					pin.Low()
-					pin.Input()
-					signal.On = false
-				}
-
-				return m.Send(fmt.Sprintf("Switched %s light %s", payload, map[bool]string{true: "on", false: "off"}[signal.On]))
-
 			default:
-				return m.Send("Usage: /s red|amber|green")
+				return m.Send("")
 			}
 		})
 
